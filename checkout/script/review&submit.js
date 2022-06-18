1bcf0a2fb8b2df220baeed2ecf024cc125d6ad2f
Deepak tiwari <atrivedi8988@gmail.com>
2022-06-18 14:35:03 +0530

    update

--- checkout/script/review&submit.js
+++ checkout/script/review&submit.js
@@ -6,14 +6,18 @@ process_div.innerHTML = process()
 
 let userDataObj = JSON.parse(localStorage.getItem("userInfo"))||{}
 
-import {append,productAppend} from "../components/append.js"
+import {append,productAppend,orderDetails} from "../components/append.js"
 let user_address =  document.getElementById("user_address_A");
 append(user_address,userDataObj)
 
 
-let userProDataArray = JSON.parse(localStorage.getItem("userProData"))
+let userProDataArray = JSON.parse(localStorage.getItem("cart"))
 let product_details = document.getElementById("product_details_A")
 productAppend(userProDataArray,product_details)
+
+let orer_value = JSON.parse(localStorage.getItem("order_value"))
+let orderDetails_A = document.getElementById("orderDetails_A")
+orderDetails(orer_value,orderDetails_A)
 
 
 
